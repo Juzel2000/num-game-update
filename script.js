33c1fc735653cf7a59676673b54ac3b612b6697e
Juzel2000 <muhammedjuzel2000@gmail.com>
2023-02-12 16:54:54 +0530

    f

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let score = 20;
 var secretnumber = Math.trunc(Math.random() * 20) + 1;
 let highscore = 0;
 
+//edit added
+document.querySelector('.number').textContent = secretnumber;         
 
 
 document.querySelector('.again').addEventListener('click', function () {
@@ -16,7 +18,9 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.score').textContent = score;
   document.querySelector('main').style.backgroundColor='black';
   document.querySelector('.message').textContent = 'start Gussing';
-  document.querySelector('.number').textContent = '?';
+
+  //edit changed as comment
+  // document.querySelector('.number').textContent = '?';             3333
 
 
 
@@ -26,6 +30,7 @@ document.querySelector('.again').addEventListener('click', function () {
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
+  //edited changed as cmnt
   console.log(guess, typeof guess);
 
   if (!guess) {
@@ -44,6 +49,11 @@ document.querySelector('.check').addEventListener('click', function () {
   } 
   else if (guess !== secretnumber) {
     score--;
+    //hint item
+    
+
+
+
     if(score>0){
       document.querySelector('.message').textContent =
       guess > secretnumber? 'too high': 'too low';
@@ -91,3 +101,18 @@ document.querySelector('.check').addEventListener('click', function () {
   // }
   
 });
+
+let m=document.querySelector('.check')
+console.log(m);
+
+var timesClicked = 0;
+
+// $("#menu_button").click(function() {
+//     timesClicked++;
+
+//     if (timesClicked>1) {
+//         //run second function
+//     } else {
+//         //run first function
+//     }
+// })
